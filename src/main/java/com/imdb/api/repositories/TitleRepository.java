5c32b5924e73d543bf794bbc8708b68b7f0ec58d
Yaser Ghaderipour <yaser.ghaderipour@gmail.com>
2025-04-04 21:59:38 +0330

    Add count query

--- src/main/java/com/imdb/api/repositories/TitleRepository.java
+++ src/main/java/com/imdb/api/repositories/TitleRepository.java
@@ -10,9 +10,12 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface TitleRepository extends JpaRepository<Title, String> {
 
-    @NativeQuery(value =
-            "SELECT title.* FROM title " +
-                    "INNER JOIN crew ON title.tconst = crew.tconst AND crew.directors = crew.writers " +
-                    "INNER JOIN person ON crew.directors like '%' + person.primaryName + '%' AND person.deathYear like '%\\N%' ")
+    @NativeQuery(
+            value =
+                    "SELECT title.* FROM title INNER JOIN crew ON title.tconst = crew.tconst AND crew.directors = crew.writers " +
+                            "INNER JOIN person ON crew.directors like '%' + person.primaryName + '%' AND person.deathYear like '%\\N%' ",
+            countQuery =
+                    "SELECT title.* FROM title INNER JOIN crew ON title.tconst = crew.tconst AND crew.directors = crew.writers " +
+                            "INNER JOIN person ON crew.directors like '%' + person.primaryName + '%' AND person.deathYear like '%\\N%' ")
     Page<Title> findTitlesWithSameAndAliveCrew(Pageable pageable);
 }
